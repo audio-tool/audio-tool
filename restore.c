2992c7efb3ef403770e8b04b9817f1e956254574
Gabriel M. Beddingfield <gabrbedd@ti.com>
2012-04-25 12:07:59 -0500

    restore: Refactor to use new mixer_cache API.

--- restore.c
+++ restore.c
@@ -48,94 +48,9 @@
 
 #include "config.h"
 #include "restore.h"
+#include "mixer_cache.h"
 
 #define BUFSIZE (1L << 15)
-
-/* In kernel this is typically 44 */
-#define CTL_NAME_MAX_LEN 256
-
-struct mixer_control_info {
-	char name[CTL_NAME_MAX_LEN];
-	unsigned id;
-	enum mixer_ctl_type type;
-	int touch;
-};
-
-struct mixer_db {
-	struct mixer_control_info data;
-	struct mixer_db *next;
-};
-
-void mixer_db_push_back(struct mixer_db *db, struct mixer_db *node)
-{
-	while (db->next)
-		db = db->next;
-	node->next = 0;
-	db->next = node;
-}
-
-void mixer_db_reset_touch(struct mixer_db *node)
-{
-	while (node) {
-		node->data.touch = 0;
-		node = node->next;
-	}
-}
-
-void mixer_db_touch(struct mixer_db *node, unsigned id)
-{
-	if (id < 0)
-		return;
-
-	while (node) {
-		if (node->data.id == id) {
-			node->data.touch = 1;
-			break;
-		}
-		node = node->next;
-	}
-}
-
-int mixer_db_create(struct mixer *mixer, struct mixer_db **db)
-{
-	struct mixer_ctl *ctl;
-	struct mixer_db *node;
-	const char* name;
-	size_t count = mixer_get_num_ctls(mixer);
-	size_t n;
-
-	if (!count)
-		return 1;
-
-	*db = 0;
-	for (n = 0 ; n < count ; ++n) {
-		node = calloc(1, sizeof(struct mixer_db));
-
-		ctl = mixer_get_ctl(mixer, n);
-		name = mixer_ctl_get_name(ctl);
-		strcpy(node->data.name, name);
-		node->data.id = n;
-		node->data.type = mixer_ctl_get_type(ctl);
-
-		if (*db) {
-			mixer_db_push_back(*db, node);
-		} else {
-			*db = node;
-		}
-	}
-
-	return 0;
-}
-
-static unsigned mixer_db_get_control_id_by_name(struct mixer_db *node, const char* name)
-{
-	while (node) {
-		if (strcmp(name, node->data.name) == 0)
-			return node->data.id;
-		node = node->next;
-	}
-	return -1;
-}
 
 static int read_line(char *dest, size_t max, const char *src, size_t size, size_t *offset)
 {
@@ -158,9 +73,9 @@ static int read_line(char *dest, size_t max, const char *src, size_t size, size_
 
 /* Notes:
  *   - param 'line' is modified by this function
- *   - param 'db' has mixer_db_touch() called on it.
+ *   - param 'db' has mixer_cache_touch() called on it.
  */
-static void process_line(char* line, struct mixer_db *db, struct mixer *mixer)
+static void process_line(char* line, struct audio_tool_mixer_cache *db, struct mixer *mixer)
 {
 	size_t length = strlen(line);
 	size_t pos = 0, sep;
@@ -189,7 +104,7 @@ static void process_line(char* line, struct mixer_db *db, struct mixer *mixer)
 			line[sep] = '\0';
 		switch(field) {
 		case FNAME:
-			control_id = mixer_db_get_control_id_by_name(db, &line[pos]);
+			control_id = mixer_cache_get_id_by_name(db, &line[pos]);
 			if (control_id == -1) {
 				printf("Error: could not find control %s\n", &line[pos]);
 				pos = length;
@@ -231,21 +146,7 @@ static void process_line(char* line, struct mixer_db *db, struct mixer *mixer)
 
 		pos = sep + 1;
 	}
-	mixer_db_touch(db, control_id);
-}
-
-int mixer_db_audit_touch(struct mixer_db *node)
-{
-	int ret = 0;
-	while (node) {
-		if (!node->data.touch) {
-			ret = 1;
-			printf("Warning: Control #%d (%s) was not reset\n",
-			       node->data.id, node->data.name);
-		}
-		node = node->next;
-	}
-	return ret;
+	mixer_cache_touch(db, control_id);
 }
 
 int restore_main(const struct audio_tool_config *config, int argc, char **argv)
@@ -259,7 +160,7 @@ int restore_main(const struct audio_tool_config *config, int argc, char **argv)
 	char *src;
 	size_t cursor;
 	struct stat fd_stat;
-	struct mixer_db *db;
+	struct audio_tool_mixer_cache db;
 
 	if (argc != 2) {
 		printf("Usage: audio-tool restore <filename>\n");
@@ -291,17 +192,21 @@ int restore_main(const struct audio_tool_config *config, int argc, char **argv)
 		return 1;
 	}
 
-	if (mixer_db_create(mixer, &db)) {
-		printf("Could not create internal mixer database\n");
+	if (mixer_cache_init(&db)) {
+		printf("Could not initialize mixer cache\n");
+		return 1;
+	}
+	if (mixer_cache_populate(&db, mixer)) {
+		printf("Could not populate mixer cache\n");
 		return 1;
 	}
 
-	mixer_db_reset_touch(db);
+	mixer_cache_reset_touch(&db);
 	cursor = 0;
 	while(read_line(buf, BUFSIZE, src, fd_stat.st_size, &cursor)) {
-		process_line(buf, db, mixer);
+		process_line(buf, &db, mixer);
 	}
-	ret = mixer_db_audit_touch(db);
+	ret = mixer_cache_audit_touch(&db, 1);
 
 	munmap(src, fd_stat.st_size);
 	close(fd);
